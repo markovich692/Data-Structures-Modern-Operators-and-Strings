efc1f24cf8c62673177c67e815633d9a798834b3
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-22 16:38:14 -0600

    Introduces the for-of loop

--- script.js
+++ script.js
@@ -61,72 +61,72 @@
 // Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 // GOOD LUCK 😀
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const {
-  // team1,
-  // team2,
-  players: [players1, players2],
-} = game;
+// const {
+//   // team1,
+//   // team2,
+//   players: [players1, players2],
+// } = game;
 
-// console.log(team1);
-// console.log(players1);
-// console.log(team1);
-// console.log(team2);
+// // console.log(team1);
+// // console.log(players1);
+// // console.log(team1);
+// // console.log(team2);
 
-const [gk, ...fieldPlayers] = players1;
+// const [gk, ...fieldPlayers] = players1;
 
-const allPlayers = [...players1, ...players2];
+// const allPlayers = [...players1, ...players2];
 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
-const { team1, x: draw, team2 } = game.odds;
-console.log(draw);
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(draw);
 
-const printGoals = function (...players) {
-  console.log(players, players.length);
-};
+// const printGoals = function (...players) {
+//   console.log(players, players.length);
+// };
 
-printGoals(...game.scored);
+// printGoals(...game.scored);
 
-console.log(team1 < team2 && 'Team1 is more likely to win');
-console.log(team1 > team2 && 'Team2 is more likely to win');
+// console.log(team1 < team2 && 'Team1 is more likely to win');
+// console.log(team1 > team2 && 'Team2 is more likely to win');
