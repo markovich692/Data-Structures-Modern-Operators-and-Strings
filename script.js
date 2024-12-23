dcb1def6d561193ce9ee31d4dcb716c727277b58
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-23 12:15:09 -0600

    starts Challenge #2

--- script.js
+++ script.js
@@ -152,20 +152,20 @@ const restaurant = {
 //   console.log(`day ${day[0] + 1}:${day[1]}`);
 // }
 
-const openingHours = {
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+// const openingHours = {
+//   thu: {
+//     open: 12,
+//     close: 22,
+//   },
+//   fri: {
+//     open: 11,
+//     close: 23,
+//   },
+//   sat: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
 
 //Property Names
 // const properties = Object.keys(openingHours);
@@ -180,19 +180,111 @@ const openingHours = {
 // console.log(sign);
 
 //Property values
-const propertyValues = Object.values(openingHours);
-console.log(propertyValues);
+// const propertyValues = Object.values(openingHours);
+// console.log(propertyValues);
 
 //Entire object
-const entireObject = Object.entries(openingHours);
+// const entireObject = Object.entries(openingHours);
+// console.log(entireObject);
 
-let sign = `We are open ${entireObject.length} days a week: `;
+// let sign = `We are open ${entireObject.length} days a week: `;
 
-for (const [key, value] of entireObject) {
-  const { open, close } = value;
+// for (const [key, { open, close }] of entireObject) {
+//   sign = sign + `on ${key} we are open from ${open} to ${close}, `;
+// }
 
-  sign = sign + `on ${key} we are open from ${open} to ${close}, `;
-}
+// sign = sign + `enjoy your meal!`;
+// console.log(sign);
 
-sign = sign + `enjoy your meal!`;
-console.log(sign);
+//CHallenge 2
+
+// Coding Challenge #2
+// Let's continue with our football betting app! Keep using the 'game' variable from before.
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console, along with
+// the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied
+// how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in an ice formatted way,exactly like this:
+// Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw").
+// Hint: Note how the odds and the game objects have the same property names 😉
+// 4. Bonus:Create an object called 'scorers' which contains the names of the players who scored
+//  as properties, and the number of goals as the value. In this game, it will look like this:
+//      {
+//        Gnarby: 1,
+//        Hummels: 1,
+//        Lewandowski: 2
+// }
+// GOOD LUCK 😀
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const {
+  // team1,
+  // team2,
+  players: [players1, players2],
+} = game;
+
+// console.log(team1);
+// console.log(players1);
+// console.log(team1);
+// console.log(team2);
+
+const [gk, ...fieldPlayers] = players1;
+
+const allPlayers = [...players1, ...players2];
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(draw);
+
+const printGoals = function (...players) {
+  console.log(players, players.length);
+};
+
+printGoals(...game.scored);
+
+console.log(team1 < team2 && 'Team1 is more likely to win');
+console.log(team1 > team2 && 'Team2 is more likely to win');
