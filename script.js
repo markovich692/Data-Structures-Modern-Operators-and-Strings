f7415593afdf80a5d65f21860fc15ccdd56f9103
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-26 09:29:48 -0600

    introduces challenges #3 on Data Structures

--- script.js
+++ script.js
@@ -312,46 +312,46 @@ const restaurant = {
 // }
 // GOOD LUCK 😀
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
 //1
 // for (const playerName of game.scored.entries()) {
@@ -404,10 +404,28 @@ const game = {
 // }
 
 //4 Bonus
-const scorers = {};
+// const scorers = {};
 
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-}
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
 
-console.log(scorers.Gnabry);
+// console.log(scorers.Gnabry);
+
+//CODING CHALLENGE #4
+
+// Let's continue with our football betting app! This time, we have a map
+// called 'gameEvents' (see below) with a log of the events that happened
+// during the game. The values are the events themselves, and the keys are
+// the minutes in which each event happened (a football game has 90 minutes
+// plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened
+// (no duplicates)
+// 2. After the game has finished,is was found that the yellow card from
+// minute 64 was unfair.So remove this event from the game events log.
+// 3. Compute and log the following string to the console:"An event happened,
+// on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console,marking whether
+// it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽   GOAL GOOD LUCK 😀
